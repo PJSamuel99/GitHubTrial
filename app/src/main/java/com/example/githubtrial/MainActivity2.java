0db94aa80831041da1adce49c1d72083b040e0a4
PJSamuel99 <priscilla.samuel981@gmail.com>
2020-10-16 21:05:51 +0530

    test

---
+++ app/src/main/java/com/example/githubtrial/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.example.githubtrial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button view;
+    String  value;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+        view =findViewById(R.id.button);
+        view.setOnClickListener(View ->v){
+
+        }
+    }
+}
